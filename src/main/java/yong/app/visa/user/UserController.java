39b44c776a1cf2bb1abeb20270f38523960c5c6c
Yongseok1991 <dobest0998@gmail.com>
2023-03-07 22:49:51 +0900

    제이쿼리, 테일윈드 추가

---
+++ src/main/java/yong/app/visa/user/UserController.java
@@ -0,0 +1,15 @@
+package yong.app.visa.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/visa")
+public class UserController {
+
+    @GetMapping("/users")
+    public String index() {
+        return "user/index";
+    }
+}
