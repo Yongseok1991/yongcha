c3be95662eccff3454f2b8cc6454ce388c39420b
Yongseok1991 <dobest0998@gmail.com>
2023-05-05 11:27:14 +0900

    등록자 수정자

--- src/main/java/yong/app/global/config/AuditorAwareImpl.java
+++ src/main/java/yong/app/global/config/AuditorAwareImpl.java
@@ -29,9 +29,7 @@ public class AuditorAwareImpl implements AuditorAware<Long> {
         if(authentication != null && !"anonymousUser".equals(authentication.getPrincipal())) {
 
             PrincipalDetails details = (PrincipalDetails) authentication.getPrincipal();
-            Long regId = details.getUser().getUid();
-            Long updtId = details.getUser().getUid();
-            return Optional.ofNullable(updtId);             // ofNullable : return null
+            return Optional.of(details.getUser().getUid());             // ofNullable : return null
 
         } else {
             return null;
