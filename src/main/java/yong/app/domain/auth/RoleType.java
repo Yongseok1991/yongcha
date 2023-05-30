fd314bd873a1c6e5fea6d1a40c2f9094c88efae3
hongdahyeon <hyeon8287@gmail.com>
2023-05-30 16:24:12 +0900

    commit - user(CRU)

--- src/main/java/yong/app/domain/auth/RoleType.java
+++
@@ -1,6 +0,0 @@
-package yong.app.domain.auth;
-
-public enum RoleType {
-    ROLE_ADMIN,
-    ROLE_USER
-}
--- src/main/java/yong/app/domain/post/comment/YongCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package yong.app.domain.post.comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface YongCommentRepository extends JpaRepository<YongComment, Long> {
-}
-
