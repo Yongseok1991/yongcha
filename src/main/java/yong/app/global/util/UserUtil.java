3dab7dbd9de0d4803c0c52f4134223ccdcfc4654
yongseok1991 <dobest09998@gmail.com>
2023-06-08 17:51:47 +0900

    private 생성자 lombok 대체

--- src/main/java/yong/app/global/util/UserUtil.java
+++ src/main/java/yong/app/global/util/UserUtil.java
@@ -1,9 +1,11 @@
 package yong.app.global.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.security.core.Authentication;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class UserUtil {
-    private UserUtil() {}
 
     public static boolean isAuthenticated(Authentication authentication) {
         return authentication != null &&
